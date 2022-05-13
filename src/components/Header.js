bd3c14eef2da62cb188019b0b62282f6a832da81
Samiya Maity <maitysamiya@gmail.com>
2022-05-14 02:23:27 +0530

    Static page for post is added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {GoVerified} from  "react-icons/go"
-import {BsChevronDown} from "react-icons/bs"
+import {GrDown} from "react-icons/gr"
 
 function Header(){
     return(
@@ -12,7 +12,7 @@ function Header(){
             <span className="icons"><GoVerified/></span>
             <span className="userName">@SamsungMobile</span>
             <span className="time">&#183; 3h</span>
-            <span className="downArrow"><BsChevronDown/></span>
+            <span className="downArrow"><GrDown/></span>
         </div>
         <div>
         <a href="#/" className="hashtag">
